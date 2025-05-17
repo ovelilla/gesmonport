e1640dd95242185903c9e0ec7e11491c2c805774
Oscar Velilla <o.velillaa@gmail.com>
2025-05-17 13:12:44 +0200

    updates

--- src/app/(auth)/(pages)/reset-password/reset-password.container.tsx
+++ src/app/(auth)/(pages)/reset-password/reset-password.container.tsx
@@ -1,4 +1,6 @@
 "use client";
+// Vendors
+import { Suspense } from "react";
 // Components
 import { ButtonLink } from "@/components/ui/button-link";
 import { ButtonLoading } from "@/components/ui/button-loading";
@@ -39,87 +41,89 @@ const ResetPasswordContainer = () => {
   } = ResetPasswordHook();
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Restablece tu contraseña</CardTitle>
-        <CardDescription>
-          Introduce tu nueva contraseña a continuación.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleSubmit)}
-            className="flex flex-col gap-6"
-          >
-            <div className="flex flex-col gap-4">
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormLabel htmlFor={field.name}>
-                        Nueva contraseña
-                      </FormLabel>
-                      <div className="relative">
-                        <FormControl>
-                          <Input
-                            {...field}
-                            autoComplete="new-password"
-                            className="pr-12"
+    <Suspense>
+      <Card>
+        <CardHeader>
+          <CardTitle>Restablece tu contraseña</CardTitle>
+          <CardDescription>
+            Introduce tu nueva contraseña a continuación.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="flex flex-col gap-6"
+            >
+              <div className="flex flex-col gap-4">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormLabel htmlFor={field.name}>
+                          Nueva contraseña
+                        </FormLabel>
+                        <div className="relative">
+                          <FormControl>
+                            <Input
+                              {...field}
+                              autoComplete="new-password"
+                              className="pr-12"
+                              disabled={loading}
+                              id={field.name}
+                              placeholder="Introduce una contraseña segura"
+                              type={showPassword ? "text" : "password"}
+                            />
+                          </FormControl>
+                          <ButtonTogglePassword
+                            aria-label={
+                              showPassword
+                                ? "Ocultar contraseña"
+                                : "Mostrar contraseña"
+                            }
                             disabled={loading}
-                            id={field.name}
-                            placeholder="Introduce una contraseña segura"
-                            type={showPassword ? "text" : "password"}
+                            onClick={handleToggleShowPassword}
+                            showPassword={showPassword}
                           />
-                        </FormControl>
-                        <ButtonTogglePassword
-                          aria-label={
-                            showPassword
-                              ? "Ocultar contraseña"
-                              : "Mostrar contraseña"
-                          }
-                          disabled={loading}
-                          onClick={handleToggleShowPassword}
-                          showPassword={showPassword}
-                        />
-                      </div>
-                      <FormMessage />
-                      <PasswordStrength value={field.value}>
-                        <PasswordStrengthBar />
-                        <PasswordStrengthRules>
-                          <PasswordStrengthRule rule="length">
-                            Mínimo 6 caracteres
-                          </PasswordStrengthRule>
-                          <PasswordStrengthRule rule="number">
-                            Un número
-                          </PasswordStrengthRule>
-                          <PasswordStrengthRule rule="uppercase">
-                            Una letra mayúscula
-                          </PasswordStrengthRule>
-                          <PasswordStrengthRule rule="special">
-                            Un carácter especial
-                          </PasswordStrengthRule>
-                        </PasswordStrengthRules>
-                      </PasswordStrength>
-                    </FormItem>
-                  );
-                }}
-              />
-            </div>
-            <ButtonLoading type="submit" loading={loading}>
-              Restablecer contraseña
-            </ButtonLoading>
-          </form>
-        </Form>
-      </CardContent>
-      <CardFooter>
-        <ButtonLink linkProps={{ href: "/signin" }}>
-          Volver a iniciar sesión
-        </ButtonLink>
-      </CardFooter>
-    </Card>
+                        </div>
+                        <FormMessage />
+                        <PasswordStrength value={field.value}>
+                          <PasswordStrengthBar />
+                          <PasswordStrengthRules>
+                            <PasswordStrengthRule rule="length">
+                              Mínimo 6 caracteres
+                            </PasswordStrengthRule>
+                            <PasswordStrengthRule rule="number">
+                              Un número
+                            </PasswordStrengthRule>
+                            <PasswordStrengthRule rule="uppercase">
+                              Una letra mayúscula
+                            </PasswordStrengthRule>
+                            <PasswordStrengthRule rule="special">
+                              Un carácter especial
+                            </PasswordStrengthRule>
+                          </PasswordStrengthRules>
+                        </PasswordStrength>
+                      </FormItem>
+                    );
+                  }}
+                />
+              </div>
+              <ButtonLoading type="submit" loading={loading}>
+                Restablecer contraseña
+              </ButtonLoading>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter>
+          <ButtonLink linkProps={{ href: "/signin" }}>
+            Volver a iniciar sesión
+          </ButtonLink>
+        </CardFooter>
+      </Card>
+    </Suspense>
   );
 };
 
