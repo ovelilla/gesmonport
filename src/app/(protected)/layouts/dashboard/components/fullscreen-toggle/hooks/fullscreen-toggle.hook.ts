73b5b90af4bc4fc296b845e5be3522c374411f1d
Oscar Velilla <o.velillaa@gmail.com>
2025-05-22 15:29:38 +0200

    updates

--- src/app/(protected)/layouts/dashboard/components/fullscreen-toggle/hooks/fullscreen-toggle.hook.ts
+++ src/app/(protected)/layouts/dashboard/components/fullscreen-toggle/hooks/fullscreen-toggle.hook.ts
@@ -1,22 +1,22 @@
 // Vendors
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 // Handlers
 import { FullscreenToggleHandlers } from "../handlers/fullscreen-toggle.handlers";
 
 const FullscreenToggleHook = () => {
   const [isFullscreen, setIsFullscreen] = useState(false);
 
-  const { handleFullscreenChange, handleToggleFullscreen } =
-    FullscreenToggleHandlers({
-      setIsFullscreen,
-    });
+  const { handleFullscreenChange, handleToggleFullscreen } = useMemo(
+    () => FullscreenToggleHandlers({ setIsFullscreen }),
+    [setIsFullscreen],
+  );
 
   useEffect(() => {
     document.addEventListener("fullscreenchange", handleFullscreenChange);
     return () => {
       document.removeEventListener("fullscreenchange", handleFullscreenChange);
     };
-  }, []);
+  }, [handleFullscreenChange]);
 
   return {
     handleToggleFullscreen,
